18bf059548a23975d85cb5a265eab25c0b6ceb14
Neba Funwi/gabga <gabs@nebas-mbp.home>
2019-10-14 02:20:37 +0200

    Added logo dimensions

--- resources/views/backend/includes/header.blade.php
+++ resources/views/backend/includes/header.blade.php
@@ -2,8 +2,8 @@
     <button class="navbar-toggler sidebar-toggler d-lg-none mr-auto" type="button" data-toggle="sidebar-show">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand" href="#">
-        <img class="navbar-brand-full" src="{{ setting('site.logo') }}" width="100" height="30" alt="CoreUI Logo">
+    <a class="navbar-brand bg-warning" href="#">
+        <img class="navbar-brand-full" src="{{ setting('site.logo') }}" width="150">
     </a>
     <button class="navbar-toggler sidebar-toggler d-md-down-none" type="button" data-toggle="sidebar-lg-show">
         <span class="navbar-toggler-icon"></span>
